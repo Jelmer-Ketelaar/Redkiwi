4cd47eb99ae4fae8a10eab65c05f78e6b172f523
Jelmer Ketelaar <jelmer@ketelaar.me>
2023-10-05 21:38:15 +0200

    Implement tests for word search

---
+++ tests/SearchWordTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require '../src/searchWord.php';
+
+class SearchWordTest extends TestCase
+{
+    private array $board = [
+        ['a', 'b', 'c', 'd'],
+        ['d', 'k', 'l', 'm'],
+        ['m', 'f', 'b', 's']
+    ];
+
+    // The test cases for when the word has been found
+    public function testSearchWordFound()
+    {
+        $this->assertTrue(searchWord($this->board, 'abcd'));
+        $this->assertTrue(searchWord($this->board, 'abcl'));
+        $this->assertTrue(searchWord($this->board, 'admfbl'));
+    }
+
+    // The test cases for when the word has not been found
+    public function testSearchWordNotFound()
+    {
+        $this->assertFalse(searchWord($this->board, 'abcc'));
+        $this->assertFalse(searchWord($this->board, 'abcdc'));
+        $this->assertFalse(searchWord($this->board, 'dklml'));
+    }
+}
